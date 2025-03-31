45e7ce8ee7d20343e410696dfe5dd846a6be8037
Anirudh Singh <anirudh.singh.7773@gmail.com>
2025-03-31 11:49:30 +0530

    changes except hamburger

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".header__hamburger").addEventListener("click", function () {
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelector(".header__hamburger").addEventListener("click", function () {
 
-    // Adds class to make position relative from absolute of header.
-    document.querySelector(".header").classList.toggle("header__show");
+//     // Adds class to make position relative from absolute of header.
+//     document.querySelector(".header").classList.toggle("header__show");
 
-    // Show and hide elements with slide animation.
-    document.querySelector(".header__top").classList.toggle("show");
-    document.querySelector(".header__nav-bar").classList.toggle("show");
-    document.querySelector(".header__cta").classList.toggle("show");
-  });
-});
+//     // Show and hide elements with slide animation.
+//     document.querySelector(".header__top").classList.toggle("show");
+//     document.querySelector(".header__nav-bar").classList.toggle("show");
+//     document.querySelector(".header__cta").classList.toggle("show");
+//   });
+// });
