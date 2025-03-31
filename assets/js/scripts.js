e40137fc61155a3e1952715fab99b82c7b207ac6
Anirudh Singh <anirudh.singh.7773@gmail.com>
2025-03-31 15:54:29 +0530

    created hamburger menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,54 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.querySelector(".header__hamburger").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
 
-//     // Adds class to make position relative from absolute of header.
-//     document.querySelector(".header").classList.toggle("header__show");
+  // Select dropdown icons 
+  const icon_dropdown = document.querySelectorAll(".header__dropdown > i");
 
-//     // Show and hide elements with slide animation.
-//     document.querySelector(".header__top").classList.toggle("show");
-//     document.querySelector(".header__nav-bar").classList.toggle("show");
-//     document.querySelector(".header__cta").classList.toggle("show");
-//   });
-// });
+  // Select header nav bar
+  const nav_bar = document.querySelector(".header__nav-bar");
+
+  // Select header cta button
+  const header_cta = document.querySelector(".header__cta-btn");
+
+  // Select header
+  const header = document.querySelector(".header");
+
+  // Select primary header
+  const header_primary = document.querySelector(".header__primary");
+
+  if (nav_bar && icon_dropdown[1]) {
+    
+    // Handles Secondary Menu display
+    icon_dropdown[1].addEventListener("click", function () {
+      if (window.getComputedStyle(nav_bar).display === "none") {
+        icon_dropdown[1].classList.add("fa-xmark");
+        icon_dropdown[1].classList.remove("fa-bars");
+        header.style.position = "relative";
+        nav_bar.classList.add("active");
+        nav_bar.classList.remove("inactive");
+        header_cta.classList.add("active");
+        header_cta.classList.remove("inactive");
+      }
+      else {
+        icon_dropdown[1].classList.add("fa-bars");
+        icon_dropdown[1].classList.remove("fa-xmark");
+        header.style.position = "absolute"
+        nav_bar.classList.add("inactive");
+        nav_bar.classList.remove("active");
+        header_cta.classList.add("inactive");
+        header_cta.classList.remove("active");
+      }
+    });
+
+    // Handles primary menu display
+    icon_dropdown[0].addEventListener("click", function () {
+      if (window.getComputedStyle(header_primary).display === "block") {
+        header_primary.classList.add("inactive");
+        header_primary.classList.remove("active");
+      }
+      else {
+        header_primary.classList.add("active");
+        header_primary.classList.remove("inactive");
+      }
+    });
+  }
+});
